225c2bf1ef4924fc3d18a1d714e54df14cdc727a
70000Abai <132120480+70000Abai@users.noreply.github.com>
2023-04-29 09:44:44 +0500

    Add files via upload

---
+++ assignments.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+
+func main (){
+
+	x = 1 // named variable
+	*p = true //indirect variable
+	person.name = "bob" //struct field
+	count[x] = count[x] * scale //array or slice or map element
+}
+
+*/
+
+//----------------------------
+
